1585401eecc2b274c18fadde2bc3cbb0afdbc0df
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-10-24 11:09:47 -0700

    added Routes component

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,12 +1,14 @@
 import data from "./data.json";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./actionTypes";
+
 const INITIAL_STATE = {products: data.products, cart: [{quantity:0}]};
 
 const rootReducer = (state=INITIAL_STATE, action) => {
     switch(action.type) {
-        case "ADD_TO_CART":
+        case ADD_TO_CART:
          
             return {...state, cart: [...state.cart, {...state.products[action.id], id : action.id, quantity : +action.quantity}]};
-        case "REMOVE_FROM_CART":
+        case REMOVE_FROM_CART:
          
             return {...state, cart: state.cart.filter(item => item.id !== action.id)}
         default:
